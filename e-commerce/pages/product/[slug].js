d7b9304a868eceb23b510ee9b7acc62e34a8972a
Sifiso Patel <sfsptl42@gmail.com>
2022-05-02 22:21:31 +0200

    working on the product display

--- e-commerce/pages/product/[slug].js
+++ e-commerce/pages/product/[slug].js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { client, urlFor } from '../../lib/client'
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
 
 const ProductDetails = ({ product, products }) => {
 
@@ -20,6 +21,32 @@ const ProductDetails = ({ product, products }) => {
                     </div>
                     <div className='product-details-desc'>
                         <h1>{name}</h1>
+                    </div>
+                    <div className="reviews">
+                        <div>
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiOutlineStar />
+
+                        </div>
+                        <p>
+                            (20)
+                        </p>
+
+                    </div>
+                    <h4>Details:</h4>
+                    <p>{details}</p>
+                    <p className='price'>${price}</p>
+                    <div className="quantity">
+                        <h3>Quantity: </h3>
+                        <p className='quantity-desc'>
+                            <span className='minus' onClick=""><AiOutlineMinus /></span>
+                            <span className='num' onClick="">0</span>
+                            <span className='plus' onClick=""><AiOutlinePlus /></span>
+                        </p>
                     </div>
                 </div>
 
